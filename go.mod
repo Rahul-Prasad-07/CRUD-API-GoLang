2fcd1cc34edf87643aa8a816a2ab4c20177eb7bd
Rahul-Prasad-07 <prasadrahulprn3@gmail.com>
2023-05-09 14:44:51 +0530

    initialized go mode & write movie struct in main.go

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Rahul-Prasad-07/CRUD-API-GoLang
+
+go 1.20
+
+require github.com/gorilla/mux v1.8.0 // indirect
---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+// for formatting and printing
+// for logging
+// for encode my data when send to postman
+// to recate new movie at random
+// for creating server
+
+// for routing : for beginner phase we are not using complex concept like routing
+
+// we are not using databse, we are using struct for store data
+// struct of : movie and Director
+
+type Movie struct {
+	ID       string    `json:"id`        // json for encode data
+	Isbn     string    `json:"isbn"`     // Isbn is uniuqe id for every movie
+	Title    string    `json:"title"`    // title of movie
+	Director *Director `json:"director"` // director of movie : * used for other struct is associated with that
+
+}
+
+// movie and director are going to associated with each other : every movie has director
+type Director struct {
+}
