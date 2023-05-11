d287c273a6b12077229199a586d8cc9622030a87
Rahul-Prasad-07 <prasadrahulprn3@gmail.com>
2023-05-11 11:06:38 +0530

    write main func

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
-// for formatting and printing
-// for logging
-// for encode my data when send to postman
-// to recate new movie at random
-// for creating server
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"math/rand"
+	"encoding/json"
+
+	"github.com/gorilla/mux"
+)
 
 // for routing : for beginner phase we are not using complex concept like routing
 
@@ -23,4 +28,25 @@ type Movie struct {
 type Director struct {
 	Firstname string `json:"firstname"`
 	Lastname  string `json:"lastname"`
+}
+
+var movies []Movie
+
+func main() {
+
+	r := mux.NewRouter()
+
+	// Mock Data - @todo - implement DB
+	movies = append(movies, Movie{ID: "1", Isbn: "438227", Title: "Star Wars-1", Director: &Director{Firstname: "John", Lastname: "Doe"}})
+	movies = append(movies, Movie{ID: "2", Isbn: "454555", Title: "Star Wars-2", Director: &Director{Firstname: "Steve", Lastname: "Smith"}})
+
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Printf("Starting server at port 8000\n")
+	log.Fatal(http.ListenAndServe(":8000", r))
+
 }
